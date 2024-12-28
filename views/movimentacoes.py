ee46cfb68ffc14e9e94749fc52ffe4a7eb5cafba
Finao01 <vinicius.x.santos11@gmail.com>
2024-12-28 22:32:40 +0000

    Updating app.py

--- views/movimentacoes.py
+++ views/movimentacoes.py
@@ -1,5 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(page_title="Movimentações de Clientes", page_icon="💰", layout="wide")
 
 st.title("Operações")
+rendafixa_df = pd.read_excel("Bases/Planilha de Movimentação.xlsx","Renda Fixa")
+st.dataframe(rendafixa_df)
