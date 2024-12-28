df80e49bd80c78c5f9cadd12824b7fa6416d4750
Finao01 <vinicius.x.santos11@gmail.com>
2024-12-28 22:04:37 +0000

    Updating app.py

--- views/page_1.py
+++ views/page_1.py
@@ -3,5 +3,5 @@ import streamlit as st
 st.set_page_config(page_title="Página 1", page_icon="💰", layout="wide")
 
 st.title("Página 1")
-
+st.subheader("Legenda Menor")
 st.write("Essa é a página 1")
